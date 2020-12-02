aef9bfd47127a8993531eaa048b2e0487d9572a5
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-12-01 23:29:42 -0600

    added header with index

---
+++ employee/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div style= {{textAlign: "center", fontFamily: "cursive"}}>
+        <h1>Employee Directory</h1>
+        </div> 
+    )
+}
+
+export default Header;
